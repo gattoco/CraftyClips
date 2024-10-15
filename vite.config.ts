a1fab4816587acdbda28903f7d6876233d0e268e
gattoco <120089903+gattoco@users.noreply.github.com>
2024-10-15 12:57:27 +1100

    chore(github): add env for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@ import solidPlugin from 'vite-plugin-solid';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [solidPlugin()],
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
     },
   },
+  base: mode === 'github-pages' ? '/CraftyClips/' : '/',
   build: {
     target: 'esnext',
     outDir: 'dist'
   }
-});
+}));
