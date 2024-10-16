9b038f8fb95a3acf006d14d877ec1b9a14eeff88
gattoco <gattoco@users.noreply.github.com>
2024-10-16 22:02:34 +0000

    deploy: 8d300b67fd40d42f4326d572f2a67b1692cd6eda

---
+++ assets/index-BJTvGLWw.js
@@ -0,0 +1,1 @@
+import{f as t,i as r,t as s}from"./index-eSZ0dSPy.js";var i=s("<code>");const m=()=>{const a=t();return["Ima clip queue with id of ",(()=>{var e=i();return r(e,()=>a.id),e})()]};export{m as default};
