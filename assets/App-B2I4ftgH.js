7835a182c91d2c665784e884468ecb4130ede22c
gattoco <gattoco@users.noreply.github.com>
2024-10-28 13:16:36 +0000

    deploy: eb8bb72628c39b78b33153c799a07b6a34a1ff5f

--- assets/App-B2I4ftgH.js
+++
@@ -1,1 +0,0 @@
-import{u as t,t as e}from"./index-CxIWv2we.js";var a=e("<div>Home dashboard something soemthing");const s=()=>(t(),a());export{s as default};
---
+++ assets/App-IKSefrMi.js
@@ -0,0 +1,1 @@
+import{u as t,t as e}from"./index-CPbyo8j3.js";var a=e("<div>Home dashboard something soemthing");const s=()=>(t(),a());export{s as default};
